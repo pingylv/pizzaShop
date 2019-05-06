b302d02360529ac815060e7c826d09c7d2c07aa2
Raitis <pingy030799@gmail.com>
2019-05-06 19:04:34 +0300

    db fixed nad rake stuff

--- db/migrate/20190506145140_create_products.rb
+++ db/migrate/20190506145140_create_products.rb
@@ -1,18 +1,19 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
 
-  	  	create_table :products do |t|
+  	create_table :products do |t|
   		t.string :title
   		t.text :description
-  		t.deciaml :price
-  		t.decimal :size
+  		t.float :price
+  		t.float :size
   		t.boolean :is_spicy
   		t.boolean :is_veg
   		t.boolean :is_best_offer
   		t.string :path_to_image
-
+  		t.string :dankmeme
 
   		t.timestamps
-  
+  	end
+
   end
 end
