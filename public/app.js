3a26830c02c5cd1598f30bc4a4e22399535d6750
Raitis <pingy030799@gmail.com>
2019-05-07 19:53:04 +0300

    can add multiple products to cart

--- public/app.js
+++ public/app.js
@@ -10,5 +10,7 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You ordered pizza with id:' + id);
+	var x = window.localStorage.getItem('product_' + id);
+	x = x*1+1;
+	window.localStorage.setItem('product_' + id, x);
 }
