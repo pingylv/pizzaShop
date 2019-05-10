16a038dd64dff0e6c6d0dedd3010429c91882d10
Raitis <pingy030799@gmail.com>
2019-05-10 20:23:44 +0300

    cart dislays correct number of items

--- public/app.js
+++ public/app.js
@@ -16,6 +16,7 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function update_orders_input()
@@ -23,6 +24,12 @@ function update_orders_input()
 	var orders = cart_get_orders()
 	$('#orders_input').val(orders);
 
+}
+
+function update_orders_button()
+{
+	var text = 'cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);	
 }
 
 function cart_get_number_of_items()
