0862dd8164a2d437cff0b81cafee3b4592266aa3
Raitis <pingy030799@gmail.com>
2019-05-10 18:22:54 +0300

    java function forgetting number of items to cart

--- public/app.js
+++ public/app.js
@@ -15,3 +15,20 @@ function add_to_cart(id)
 	x = x*1+1;
 	window.localStorage.setItem(key, x);
 }
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++ )
+	{
+		var key = window.localStorage.key(i); 
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
