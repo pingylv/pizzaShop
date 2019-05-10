68de019f170c12760341aa14877248215da5190e
Raitis <pingy030799@gmail.com>
2019-05-10 20:13:21 +0300

    adding t cart adds text to field next to checkout button

--- public/app.js
+++ public/app.js
@@ -15,7 +15,14 @@ function add_to_cart(id)
 	x = x*1+1;
 	window.localStorage.setItem(key, x);
 
-	alert('number of items in youre cart: ' + cart_get_number_of_items());
+	update_orders_input();
+}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders()
+	$('#orders_input').val(orders);
+
 }
 
 function cart_get_number_of_items()
