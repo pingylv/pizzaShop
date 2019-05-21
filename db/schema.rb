bd09ef3cfb89d55c1b37a7cb2e0cff36dfc984d2
Raitis <pingy030799@gmail.com>
2019-05-21 19:25:06 +0300

    add orders model + migratio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_161429) do
+ActiveRecord::Schema.define(version: 2019_05_21_160114) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "orders_input"
+    t.text "name"
+    t.text "phone"
+    t.text "adress"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
